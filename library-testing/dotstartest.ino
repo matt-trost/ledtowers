459205b778e5765e5fc4816e4031bfecd7447a8f
Matt Trost <mtrost@stanford.edu>
2019-02-28 14:13:10 +0800

    barebones library usages

---
+++ library-testing/dotstartest.ino
@@ -0,0 +1,29 @@
+
+#include <Adafruit_DotStar.h>
+#include <SPI.h>
+
+#define NUMPIXELS 42 // Number of LEDs in strip
+#define DATAPIN    4
+#define CLOCKPIN   5
+
+Adafruit_DotStar strip = Adafruit_DotStar(
+  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
+
+
+void setup() {
+  Serial.begin(115200);
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+  for (int i = 0; i < NUMPIXELS; ++i) {
+    strip.setPixelColor(i, 255, 255, 255);
+  }
+  strip.show();
+  Serial.println("one");
+}
+
+void loop() {
+//  for (int i = 0; i < NUMPIXELS; ++i) {
+//    strip.setPixelColor(i, 255, 255, 255);
+//  }
+//  strip.show();
+}
---
+++ library-testing/fastledtest.ino
@@ -0,0 +1,23 @@
+#include <FastLED.h>
+#define NUM_LEDS 3
+
+#define DATA_PIN 4
+#define CLOCK_PIN 5
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("resetting");
+  LEDS.addLeds<APA102,DATA_PIN,CLOCK_PIN, BGR, DATA_RATE_MHZ(24)>(leds,NUM_LEDS);
+  LEDS.setBrightness(250);
+}
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; ++i) {
+    leds[i] = CRGB(255, 255, 255);
+  }
+  FastLED.show();
+  Serial.println("one");
+}
+
